2b3dac98053839c8dedee5949e17773ced35ef3d
github username <github email address>
2018-07-18 22:19:06 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+const justInvoke = (fn) => {
+  return fn()
+}
+
+const setThisWithCall = (fn, thisValue, arg) => {
+  return fn.call(thisValue, arg)
+}
+
+const setThisWithApply = (fn, thisValue, arg) => {
+  return fn.apply(thisValue, arg)
+}
+
+const returnNewFunctionOf = (fn, thisValue) => {
+  return fn.bind(thisValue)
+}
